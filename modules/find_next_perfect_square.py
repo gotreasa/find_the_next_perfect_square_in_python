904e7437d2d0facbf344043e3b149969328fecd2
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-01-09 13:01:58 +0000

    refactor: to use clearer is_integer() function

--- modules/find_next_perfect_square.py
+++ modules/find_next_perfect_square.py
@@ -6,6 +6,6 @@ def find_next_square(number: int) -> int:
         raise ValueError("❗️ Input is not an integer")
     square_root = math.sqrt(number)
 
-    if square_root % 1 == 0:
+    if square_root.is_integer():
         return (square_root + 1) ** 2
     return -1
